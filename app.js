e84bef25da4f337a13dda87781b3af27d1addf85
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2021-12-09 18:30:15 +0100

    generateur de bulle

--- app.js
+++ app.js
@@ -12,3 +12,34 @@ equal.addEventListener("click",()=>{
 clear.addEventListener("click", ()=>{
     result.textContent = " ";
 });
+
+
+/*Générateur de bulle*/
+let counter =document.querySelector('.h3') ;
+let icount=1;
+const bubbleMaker= ()=>{    
+    const bubble=document.createElement("span");
+    document.body.appendChild(bubble);
+    bubble.classList.add("bubble");
+
+    let sizes=Math.random()*200 + 100 + "px";
+    bubble.style.height=sizes;
+    bubble.style.width=sizes;
+
+    bubble.style.top=Math.random()*100 + 50 + "%";
+    bubble.style.left=Math.random()*100  + "%";
+
+    const plusMinus = Math.random() >0.5 ? 1: -1;
+    bubble.style.setProperty('--left', Math.random()*100 * plusMinus+"%"); 
+
+    bubble.addEventListener("click", ()=>{
+        bubble.remove();
+        counter.textContent=icount++;
+    })
+
+    setTimeout(()=>{
+        bubble.remove();
+    }, 8000);
+}
+
+setInterval(bubbleMaker, 1000); 
