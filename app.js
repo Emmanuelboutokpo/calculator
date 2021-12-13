3acb6c17cf5da1484b830d7f8b554a1afd152836
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2021-12-13 16:50:00 +0100

    Text typing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ clear.addEventListener("click", ()=>{
 
 /*Générateur de bulle*/
 let counter =document.querySelector('.h3') ;
-let icount=1;
+let icount=1;    
 const bubbleMaker= ()=>{    
     const bubble=document.createElement("span");
     document.body.appendChild(bubble);
@@ -41,5 +41,40 @@ const bubbleMaker= ()=>{
         bubble.remove();
     }, 8000);
 }
-
 setInterval(bubbleMaker, 1000); 
+
+/* Text typing */
+const target = document.getElementById("target");
+ let array =["développeur", "électrotechnicien", "créatif"];
+ let wordIndex =0;
+ let letterIndex = 0;
+
+ const createLetter = ()=>{
+    const letter = document.createElement("span");
+    target.appendChild(letter);
+    letter.textContent = array[wordIndex][letterIndex];
+    setTimeout(() => {
+        letter.remove();
+    },2800);
+}
+
+const loop=()=>{
+      setTimeout(() => {
+          if(wordIndex>=array.length){
+              wordIndex=0;
+              letterIndex=0;
+              loop();
+          }else  if(letterIndex < array[wordIndex].length){
+          createLetter();
+          letterIndex++
+          loop();
+      }else{
+      wordIndex++;
+      letterIndex=0
+      setTimeout(() => {
+           loop();
+      }, 2800);
+    }
+  }, 50);
+}
+loop();
