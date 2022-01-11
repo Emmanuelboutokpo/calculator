ef6d9545d2444bc52092a453d74797aaba4174b0
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2022-01-11 16:16:18 +0100

    emoji dynamique

--- app.js
+++ app.js
@@ -741,3 +741,18 @@ deletes.forEach(deleted =>{
 });
 
 });
+
+/*EMOJI DYNAMIQUE*/  
+document.body.addEventListener('mousemove',(e)=>{
+    let eyes =document.querySelectorAll(".eye");
+   
+     eyes.forEach(eye =>{
+         
+         let x =(eye.getBoundingClientRect().left) + (eye.clientWidth/2);
+         let y =(eye.getBoundingClientRect().top) + (eye.clientHeight/2);
+
+         let radian = Math.atan2(event.pageX - x, event.pageY -y);
+         let rot =(radian * (180/Math.PI)* -1 + 270);
+         eye.style.transform = "rotate(" + rot + "deg)"
+     }) ;
+})
