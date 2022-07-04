2f782706fdd552e0b63ac9539b63851c77fabafe
BOUTOKPO Emmanuel <tossedeemmanuelboutokpo@gmail.com>
2022-07-04 20:44:29 +0100

    My js projects

---
+++ tyt/app.js
@@ -0,0 +1,17 @@
+const ry= (arr,ar)=>{
+ /*  const input = document.querySelector("input");
+     const form = document.querySelector("form");
+     const arr = [];
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        const inpu=parseInt(input.value); 
+       let ru =null;
+        arr.push(inpu);
+         ru=  arr.reverse()
+        console.log(arr);
+        input.value = ""
+    }) */
+    let a= arr.concat(ar)
+    console.log(a);
+}
+ry([1,2,3,5,false],[7,9,5.])
